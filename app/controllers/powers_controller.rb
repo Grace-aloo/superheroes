2400e6ade925a92fb8e127d2d59504d3fa360441
grace_aloo <graceaaloo@gmail.com>
2023-03-27 18:37:22 +0300

    update action added in power

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,6 +1,6 @@
 class PowersController < ApplicationController\
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     def Index 
         powers = Power.all 
@@ -12,6 +12,12 @@ class PowersController < ApplicationController\
         render json: power
     end
 
+    def update 
+        power = find_power
+        power.update(power_params)
+        render json: power, status: :accepted
+    end
+
     private 
     def render_not_found_response 
         render json: {error: "Power not found"}, status: :not_found
@@ -19,5 +25,13 @@ class PowersController < ApplicationController\
 
     def find_power
         Power.find(params[:id])
+    end
+
+    def power_params 
+        params.permit(:description)
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
 end
