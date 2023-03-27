ea0995ce03e2d5e485f97e062ad84c5bb4c0cf0f
grace_aloo <graceaaloo@gmail.com>
2023-03-27 22:28:13 +0300

    chhanged some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :hero_powers
-  resources :powers
-  resources :heros
+  resources :hero_powers, only: [:create]
+  resources :powers, only [:index,:show,:update]
+  # resources :heros
+  get '/heroes', to:"heros#index"
+  get '/heroes/:id', to:"heros#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
