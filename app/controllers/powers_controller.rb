613ef7552c39d99828f526e31d8d98c9acf88dab
grace_aloo <graceaaloo@gmail.com>
2023-03-27 18:22:58 +0300

    added index and show actions in power model

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,23 @@
-class PowersController < ApplicationController
+class PowersController < ApplicationController\
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+
+    def Index 
+        powers = Power.all 
+        response json: powers
+    end
+
+    def show 
+        power = find_power
+        render json: power
+    end
+
+    private 
+    def render_not_found_response 
+        render json: {error: "Power not found"}, status: :not_found
+    end
+
+    def find_power
+        Power.find(params[:id])
+    end
 end
