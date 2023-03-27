cc8eceaacceac6f6cccfe3b2fc00d7f92e64744f
grace_aloo <graceaaloo@gmail.com>
2023-03-27 19:19:57 +0300

    index and show method added for hero

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,22 @@
 class HerosController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index 
+        heroes = Hero.all 
+        render json: heroes
+    end
+
+    def show 
+        hero = find_hero
+        render json: hero
+    end
+
+    private 
+    def render_not_found_response 
+        render json: {error: "Hero not found"}, status: :not_found
+    end
+
+    def find_power
+        Hero.find(params[:id])
+    end
 end
